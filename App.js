4b6ed643a8fd9d64d4c8b11ba844a69ff90ab829
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-11-11 14:14:06 +0100

    Define Welcome, Practice, and Results screens

--- App.js
+++ App.js
@@ -7,23 +7,50 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
+    this.state = { currentScreen: 'welcome' };
+  }
+
+  renderWelcomeScreen() {
+    return (
+      <View>
+        <Text style={styles.welcome}>Welcome to Blitz Reading!</Text>
+        <Button title="Practice" />
+      </View>
+    );
+  }
+
+  renderPracticeScreen() {
+    return (
+      <View>
+        <Text style={styles.word}>word</Text>
+        <Button title="Next Word" />
+      </View>
+    );
+  }
+
+  renderResultsScreen() {
+    return (
+      <View>
+        <Text style={styles.welcome}>Results</Text>
+        <Text style={styles.results}>Words count: 0</Text>
+        <Button title="Practice Again" />
+      </View>
+    );
+  }
+
   render() {
+    const { currentScreen } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        { currentScreen === 'welcome' && this.renderWelcomeScreen() }
+        { currentScreen === 'practice' && this.renderPracticeScreen() }
+        { currentScreen === 'results' && this.renderResultsScreen() }
       </View>
     );
   }
@@ -41,9 +68,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  word: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10,
+  },
+  results: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 30,
   },
 });
