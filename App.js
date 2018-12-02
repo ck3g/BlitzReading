7599bba51889a953e5a81891e821230a85fa9aaf
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-12-02 10:55:04 +0100

    Define tab navigation: Home, HighScores, and Settings

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import {
+  createSwitchNavigator,
+  createBottomTabNavigator,
+  createAppContainer
+} from 'react-navigation';
 
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import PracticeScreen from './src/screens/PracticeScreen';
 import ResultsScreen from './src/screens/ResultsScreen';
+import HighScoresScreen from './src/screens/HighScoresScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
 class App extends React.Component {
   render() {
@@ -17,4 +23,10 @@ const HomeNavigator = createSwitchNavigator({
   Results: ResultsScreen
 });
 
-export default createAppContainer(HomeNavigator);
+const AppNavigator = createBottomTabNavigator({
+  Home: HomeNavigator,
+  HighScores: HighScoresScreen,
+  Settings: SettingsScreen
+});
+
+export default createAppContainer(AppNavigator);
---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+
+import styles from '../styles';
+
+export default class HighScoresScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.welcome}>Settings</Text>
+        </View>
+      </View>
+    );
+  }
+}
