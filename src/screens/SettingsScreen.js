9a5856e5afdfe15351336a766747b7f82d3077c9
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-12-16 10:37:07 +0100

    Change layout for the settings screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
-
-import styles from '../styles';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default class HighScoresScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <View>
-          <Text style={styles.welcome}>Settings</Text>
+          <Text style={styles.header}>Settings</Text>
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 45,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold'
+  }
+});
