3a36606be7f41f790bae8205481749196d232932
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2019-03-17 09:18:05 +0100

    Add a custom button component

---
+++ src/components/common/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+    <TouchableOpacity style={buttonStyle} onPress={onPress}>
+      <Text style={textStyle}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 3,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 18,
+    fontWeight: '700',
+    paddingTop: 13,
+    paddingBottom: 13
+  }
+};
+
+export { Button };
